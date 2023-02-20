9b7a2975e1f7c0f378acbc8c896ba824405d7608
hassan401 <ogbeastz@hotmail.com>
2023-02-20 21:42:20 +0000

    Intial

--- mysqlcon.py
+++ mysqlcon.py
@@ -1,18 +1,54 @@
 import mysql.connector
+import mysql
 
 db = mysql.connector.connect(
-    host= "localhost",
-    user= "hassan401",
-    password = "HumaHassan8",
-    port = "3306",
-    database = "2d_platformer"
+    host="localhost",
+    user="root",
+    password="HumaHassan8",
+    port="3306",
+    database="2d_platformer"
 )
 
 mycursor = db.cursor()
 
-mycursor.execute("SELECT * FROM user")
+# class Database:
+#     def __init__(self):
+#         self.
+
+def farzad(x, y):
+    username = x
+    password = y
+    n = 4
+    mycursor.execute("""
+    INSERT INTO `2d_platformer`.`users`
+    (`userid`,
+    `username`,
+    `password`)
+    VALUES
+    (n),(username),(password);
+    """)
+
+farzad("hello","poop")
+
+mycursor.execute("""
+INSERT INTO `2d_platformer`.`users`
+(`userid`,
+`username`,
+`password`)
+VALUES
+("3","oddsdfsfp","O3dfdPe");
+""")
+mycursor.execute("SELECT * FROM users")
 
 users = mycursor.fetchall()
 
 for user in users:
     print(user)
+
+
+
+
+
+
+
+
